c0c8fd0b033e4b36c90cf84311279cace7fbf10a
Can Kavaklioglu <can.kavaklioglu@gmail.com>
2018-07-25 04:39:57 +0000

    changed tensors from list to dict

--- gtp.py
+++ gtp.py
@@ -2,13 +2,6 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from operator import add
 
-def get_tensor_index(gtp_spec, tensor_name):
-    for tensor_index, tensor in enumerate(gtp_spec['tensors']):
-        if tensor['name'] == tensor_name:
-            return tensor_index
-
-    return None
-
 def read_tensor_data(spark_session, tensor_name, cache=True, root_path='/home/sprk/shared/gctf_data/'):
     tensor_data = spark_session.read.load(root_path+tensor_name+'.csv', format="csv", sep=",", inferSchema="true", header="true")
     if cache:
@@ -16,7 +9,7 @@ def read_tensor_data(spark_session, tensor_name, cache=True, root_path='/home/sp
     return tensor_data
 
 def find_tensor_value(gtp_spec, input_tensor_name, tensor_index_values):
-    tensor_spec = gtp_spec['tensors'][get_tensor_index(gtp_spec, input_tensor_name)]
+    tensor_spec = gtp_spec['tensors'][input_tensor_name]
     tensor_data = tensor_spec['local_data']
     for row in tensor_data:
         matched_index_count = 0
@@ -32,8 +25,8 @@ def linear_index_to_tensor_index( gtp_spec, linear_index, linear_index_tensor_na
     tensor_index_values_dict = {}
     if tensor_index_tensor_name is None:
         tensor_index_tensor_name = linear_index_tensor_name
-    tensor_index_tensor_indices = gtp_spec['tensors'][get_tensor_index(gtp_spec, tensor_index_tensor_name)]['indices']
-    for index_name in gtp_spec['tensors'][get_tensor_index(gtp_spec, linear_index_tensor_name)]['indices']:
+    tensor_index_tensor_indices = gtp_spec['tensors'][tensor_index_tensor_name]['indices']
+    for index_name in gtp_spec['tensors'][linear_index_tensor_name]['indices']:
         index_cardinality = gtp_spec['config']['cardinalities'][index_name]
         value = ((linear_index / current_stride) % index_cardinality) + 1
         current_stride *= index_cardinality
@@ -53,18 +46,18 @@ def gtp(spark, gtp_spec):
     
     # get all indices
     full_tensor_name = '_gtp_full_tensor'
-    if get_tensor_index(gtp_spec, full_tensor_name) is None:
+    if full_tensor_name not in gtp_spec['tensors']:
         all_indices = set()
-        for tensor in gtp_spec['tensors']:
+        for tensor_name, tensor in gtp_spec['tensors'].iteritems():
             all_indices.update( tensor['indices'] )
         # all_indices must have fixed ordering
         all_indices = list(all_indices)
 
     # add full tensor to the tensor list
-    gtp_spec['tensors'].append( {'name':'_gtp_full_tensor', 'indices':all_indices} )
+    gtp_spec['tensors']['_gtp_full_tensor'] = {'indices':all_indices}
 
     # indices are assumed to be serialized left to right
-    for tensor in gtp_spec['tensors']:
+    for tensor_name, tensor in gtp_spec['tensors'].iteritems():
         tensor['strides'] = []
         current_stride = 1
         tensor['numel'] = 1
@@ -85,7 +78,6 @@ def gtp(spark, gtp_spec):
         # multiply them and return (linear index, output_value)
         output_value = None
         for input_tensor_name in gtp_spec['config']['inputs']:
-            tensor_config_index = get_tensor_index(gtp_spec, input_tensor_name)
             input_tensor_value = find_tensor_value(gtp_spec, input_tensor_name, full_tensor_index_values) #spark.sql(query)
             if input_tensor_value is not None:
                 if output_value is None:
@@ -115,15 +107,14 @@ def gtp(spark, gtp_spec):
 
     gctf_data_path = '/home/sprk/shared/gctf_data'
     sc = spark.sparkContext
-    rdd = sc.parallelize(xrange(gtp_spec['tensors'][get_tensor_index(gtp_spec, '_gtp_full_tensor')]['numel']))
+    rdd = sc.parallelize(xrange(gtp_spec['tensors']['_gtp_full_tensor']['numel']))
     for input_tensor_name in gtp_spec['config']['inputs']:
-        tensor_config_index = get_tensor_index(gtp_spec, input_tensor_name)
-        if 'dataframe' not in gtp_spec['tensors'][tensor_config_index]:
+        if 'dataframe' not in gtp_spec['tensors'][input_tensor_name]:
             tensor_data = read_tensor_data(spark, input_tensor_name)
             #tensor_data.createOrReplaceTempView(input_tensor_name)
-            gtp_spec['tensors'][tensor_config_index]['local_data'] = tensor_data.rdd.collect()
+            gtp_spec['tensors'][input_tensor_name]['local_data'] = tensor_data.rdd.collect()
             #print ('\n\n\n\n')
-            #print(gtp_spec['tensors'][tensor_config_index]['local_data'])
+            #print(gtp_spec['tensors'][input_tensor_name]['local_data'])
             #print ('\n\n\n\n')
         else:
             print('info: Not re-initializing %s' %input_tensor_name)
@@ -144,20 +135,17 @@ if __name__ == '__main__':
             'output' : 'gtp_test_output',
             'inputs' : [ 'gtp_test_input1', 'gtp_test_input2' ]
         },
-        'tensors' : [
-            {
-                'name' : 'gtp_test_output',
+        'tensors' : {
+            'gtp_test_output' : {
                 'indices' : [ 'i', 'j' ],
             },
-            {
-                'name' : 'gtp_test_input1',
+            'gtp_test_input1' : {
                 'indices' : [ 'i', 'k' ]
             },
-            {
-                'name' : 'gtp_test_input2',
+            'gtp_test_input2' : {
                 'indices' :  [ 'j', 'k' ]
             }
-        ]
+        }
     }
 
     spark = SparkSession.builder.appName("gtp").getOrCreate()
